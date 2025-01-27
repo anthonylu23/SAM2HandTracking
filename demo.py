c2d0329b8dcfef0ce82eedfd0ff9cf24ca91f745
Anthony Lu <anthonyyylu24@gmail.com>
2025-01-26 23:48:46 -0600

    Update demo.py
    Fixed paths

--- demo.py
+++ demo.py
@@ -1,6 +1,6 @@
 from AutoHandTracking import hands_in_video_segmentation
 
-input_path = "/Users/anthony/Documents/CS/Coding/HandMask/sam2/script/test.mp4"
-output_path = "/Users/anthony/Documents/CS/Coding/HandMask/sam2/script/output.mp4"
+input_path = ""
+output_path = ""
 
 hands_in_video_segmentation(input_path, output_path)
